f82b8599c4f56a1d9c982eec255b4421ba333025
teknixco <vic@teknixco.net>
2021-09-12 00:12:04 +0000

    added strings tests

---
+++ strings/double_char.py
@@ -0,0 +1,14 @@
+#Given a string, return a string where for every char in the original, there are two chars.
+#double_char('The') → 'TThhee'
+#double_char('AAbb') → 'AAAAbbbb'
+#double_char('Hi-There') → 'HHii--TThheerree'
+
+def double_char(str):
+  newstr = ""
+  for i in range(len(str)):
+    tempstr = str[i]*2
+    newstr += tempstr
+  return newstr
+
+print(double_char('The'))
+print(double_char('Test'))
