a14578dc6cce529dbf011ccd49a03a832205f488
teknixco <vic@teknixco.net>
2021-11-23 03:45:11 +0000

    linked list intersection

--- graphs/linkedlist_intersect.py
+++ graphs/linkedlist_intersect.py
@@ -1,14 +1,18 @@
 import sys
 def linkedlist_intersection(group, graph) -> bool:
-    print("working")
+    return (1>2)
 
-graph = []
+graph = [] #edge list representation
 for line in sys.stdin:
     if "->" in line:
         s=line.split("->")
-
-        graph.somehow_add( s[0], s[1])
-
+        tempLine=[]
+        for val in s:
+            val=val.strip()
+            tempLine.append(val)
+            s=tempLine
+        graph.append(s)
     elif "," in line:
         print(linkedlist_intersection(line.split(","), graph) )
 
+print(graph)
--- graphs/stdin.py
+++ graphs/stdin.py
@@ -1,13 +1,13 @@
 import sys
-a=sys.stdin.readlines()
-v=[]
-for i in a:
-    i=i.split("->")
-    temp=[]
-    for j in i:
-        j=j.strip()
-        temp.append(j)
-        i=temp
-    v.append(i)
-print(v)
+graph=[]
+for line in sys.stdin:
+    if "->" in line:
+      s=line.split("->")
+    tempLine=[]
+    for val in s:
+        val=val.strip()
+        tempLine.append(val)
+        s=tempLine
+    graph.append(s)
+print(graph)
 
