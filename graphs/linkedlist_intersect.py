541c45373075494737ea3ecd90de82e493a69773
teknixco <vic@teknixco.net>
2021-11-23 05:33:47 +0000

    IT WORKSgit add .git add .git add .!

--- graphs/linkedlist_intersect.py
+++ graphs/linkedlist_intersect.py
@@ -5,9 +5,23 @@ def linkedlist_intersection(group, graph) -> bool:
         val=val.strip()
         tempGroup.append(val)
         group=tempGroup
-    print(group)
-
-    return (1>2)
+    print("group",group,"graph",graph)
+    interArray=[]
+    for i in group:
+        for j in graph:
+            if i in j:
+                if i not in interArray:
+                    interArray.append(i)
+                else:
+                    print("Value already there, intersection found at",i)
+                    return True
+                print(i,j)
+            elif i not in j:
+                if i in interArray:
+                    interArray.remove(i)
+                print(i,"is not there",j)
+    print("ARR",interArray)
+    return False
 
 graph = [] #edge list representation
 for line in sys.stdin:
